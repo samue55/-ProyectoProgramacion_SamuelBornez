78252e5b72e5643f7b8dce5b055e5bf04db60057
samue55 <148970025+samue55@users.noreply.github.com>
2026-04-09 12:18:33 +0200

    versión intermedia
    Añades funciones, clases o estructuras del ejercicio.
    Deben verse cambios reales en los archivos

--- proyectointer/src/EjercicioPOO/CuentaBancaria.java
+++ proyectointer/src/EjercicioPOO/CuentaBancaria.java
@@ -1,1 +1,48 @@
+package EjercicioPOO;
+
+public class CuentaBancaria {
+	// Atributos
+	private String titular;
+	private double saldo;
+
+
+	// Constructor
+	public CuentaBancaria() {
+	this.titular = "sin titular";
+	this.saldo = 0;
+	}
+
+
+	// Constructor con parámetros
+	public CuentaBancaria(String titular, double saldo) {
+	this.titular = titular;
+	this.saldo = saldo;
+	}
+
+
+	// Get y set 
+	public String getTitular() {
+	return titular;
+	}
+
+
+	public void setTitular(String titular) {
+	this.titular = titular;
+	}
+
+
+	public double getSaldo() {
+	return saldo;
+	}
+
+
+	public void setSaldo(double saldo) {
+	this.saldo = saldo;
+	}
+
+
+
+
+
+
 
